d96fdc65771566eda0c4633845212889a8d5124a
dvbarchuk <dvbarchuk@gmail.com>
2021-10-07 14:43:11 +0300

    Initial commit add random_key

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # utils
+
+##Генерация случайного ключв для проекта
+
+    python random_key.py <number>
+    or 
+    python random_key.py
+
---
+++ random_key.py
@@ -0,0 +1,25 @@
+import string
+import random
+from sys import argv
+
+
+def generate_random_string(num = None):   
+    letters = string.ascii_letters + string.punctuation + string.digits
+    if len(argv) == 1:
+        try:
+            num = int(input('Input number of digits >> '))
+        except ValueError:
+            print("You should input digit, but okay, i will set 50 symbols for you:")
+            num = 50
+    else:
+        try:
+            num = int(argv[1])
+        except ValueError:
+            print("You should input digit, but okay, i will set 50 symbols for you:")
+            num = 50
+    print(f"Your random key within {num} symbols is:")
+    print("".join(random.choice(letters) for i in range(num)))
+
+
+if __name__ == '__main__':
+    generate_random_string()
